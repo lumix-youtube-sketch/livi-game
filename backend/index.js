a17388f94d07e10c2e5df2d74b9c7ca72d5c5fad
Матвей <matikmatematik@yandex.ru>
2026-02-11 21:28:07 +0300

    Финальное удаление устаревших опций монго

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', apiRoutes);
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
